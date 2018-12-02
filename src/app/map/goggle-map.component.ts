c60eebefb12c14428c6c3f279c28d69ec2149359
dankphetamine <asge0907@easv365.dk>
2018-12-02 17:17:08 +0100

    Fixed center map to marker position

--- src/app/map/goggle-map.component.ts
+++ src/app/map/goggle-map.component.ts
@@ -4,6 +4,8 @@ import {MapService} from '../shared/services/map.service';
 import {Rig} from '../shared/models/rig';
 import {Observable} from 'rxjs';
 import {debounceTime, distinctUntilChanged, map} from 'rxjs/operators';
+import {google, GoogleMap} from '@agm/core/services/google-maps-types';
+import {GoogleMapsAPIWrapper} from '@agm/core';
 
 @Component({
   selector: 'app-googlemap',
@@ -17,9 +19,15 @@ export class GoggleMapComponent implements OnInit {
   infoWindow;
 
   rigs: Rig[];
+  gMap: any;
   isCollapsed = false;
+  markers: any;
 
-  constructor(private rigService: RigService, private mapService: MapService ) { }
+  constructor(private rigService: RigService, private mapService: MapService) { }
+
+  protected mapReady(map) {
+    this.gMap = map;
+  }
 
   hoverOn(event) {
     console.log(event);
@@ -47,6 +55,12 @@ export class GoggleMapComponent implements OnInit {
   searchRig = (text$: Observable<string>) => text$.pipe(debounceTime(750), distinctUntilChanged(),
     map(userInput => userInput.length < 3 ? [] : this.rigs.filter(rig => rig.name.toLowerCase().startsWith(userInput.toLocaleLowerCase()))))
 
-  showMarkerOnMap() {
+  // TODO fix (finds object, needs to find object name)
+
+  showMarkerOnMap(rig: Rig) {
+    if (this.gMap) {
+      this.gMap.setCenter({ lat: rig.lat, lng: rig.lng});
+    }
+    console.log('clicked', rig, { lat: rig.lat, lng: rig.lng});
   }
 }
